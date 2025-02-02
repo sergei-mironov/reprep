257caebefdbd892a51b0b9df73144376085aa04e
Sergei Mironov <sergei.v.mironov@proton.me>
2025-02-02 22:59:21 +0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Python project with SciPy and extensive LaTeX setup";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:grwlf/nixpkgs/local17";
+    };
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+      texliveFull = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full;
+      };
+
+    in {
+      packages = {
+        x86_64-linux = self.defaultPackage.x86_64-linux;
+      };
+      defaultPackage = {
+        x86_64-linux = self.devEnvironment;
+      };
+      devShells = {
+        x86_64-linux = self.devEnvironment;
+      };
+      devEnvironment = pkgs.mkShell {
+        buildInputs = [
+          (pkgs.python3.withPackages (p: with p; [ ipython scipy ortools python-lsp-server]))
+          texliveFull
+          pkgs.xdotool
+        ];
+
+        shellHook = ''
+          # Custom shell prompt
+          export PS1="\[\033[01;34m\] python-scipy \\w \[\033[00m\]$ "
+          export PROJECT_ROOT=`pwd`
+          export PATH=$PROJECT_ROOT/sh:$PATH
+
+          echo "Development environment with SciPy, extensive LaTeX, and custom shell prompt is ready!"
+        '';
+      };
+    };
+}
