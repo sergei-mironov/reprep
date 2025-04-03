03cd22ee93977748cc52cd0b1b68eabd067e14aa
Sergei Mironov <sergei.v.mironov@proton.me>
2025-04-03 11:33:11 +0400

    Update sedlines

--- sh/sedlines.sh
+++ sh/sedlines.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-# Print lines between start and stop, ignoring empty lines
+# (1) Print the lines between the patterns; (2) Filter out the '# \{\{\{' pattern; (3) filter-out
+# leading and trailing empty lines; (4) replace space-only lines with empty lines.
 
 if test -n "$2" ; then
   B=$1
   E=$2
 elif test -n "$1" ; then
-  B="{{{ $1"
-  E="}}} $1"
+  B="${1}.*{{{"
+  E="}}}"
 else
   echo "Usage: sedlines.sh (PAT|START STOP)" >&2
   exit 1
 fi
 
-sed -n "/$B/,/$E/{ /$B/b; /$E/b; p; }" | \
+# (1) \
+sed -n "/$B/,/$E/{ /^#.*$B/b; /$E/{q;}; p; }" | \
+# (2) \
+sed "s/#[[:space:]]*{{{//" | \
+# (3) \
 sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' | \
+# (4) \
 sed '/./,$!d'
